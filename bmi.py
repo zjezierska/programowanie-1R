c6c3a0287027de1c9c0c39a5a34700abda1593d5
zjezierska <79923468+zjezierska@users.noreply.github.com>
2021-03-02 17:03:21 +0100

    bmi

--- bmi.py
+++ bmi.py
@@ -7,9 +7,9 @@ print(f"BMI = {bmi}")
 
 if bmi < 18.5:
     print("niedowaga")
-elif 18.5 < bmi < 25:
+elif 18.5 <= bmi < 25:
     print("waga prawidłowa")
-elif 25 < bmi < 30:
+elif 25 <= bmi < 30:
     print("nadwaga")
 else:
     print("otyłość")
---
+++ lineq.py
@@ -0,0 +1,6 @@
+a = float(input("a = "))
+b = float(input("b = "))
+
+x = -b/a
+
+print(f"x = {x}")
