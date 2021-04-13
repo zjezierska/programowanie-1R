2c27cd74389a3926b116e1309eca643be109bb90
zjezierska <79923468+zjezierska@users.noreply.github.com>
2021-04-13 15:09:55 +0200

    coviddata

--- coviddata.py
+++ coviddata.py
@@ -1,19 +1,28 @@
 import matplotlib.pyplot as plt
 
-cases_pol = {}
-
+cases = {}
+kraje = []
 with open("full_data.csv") as f:
     for line in f:
         dane = line.split(",")
         date = dane[0]
         country = dane[1]
-        cases = dane[2]
-        if country == "Poland":
-            cases_pol[date] = cases
+        if country not in kraje:
+            print(country)
+            kraje.append(country)
 
-x = cases_pol.keys()
-y = cases_pol.values()
-plt.plot(x, y)
-plt.xlabel("data")
-plt.ylabel("ilość zachorowań")
-plt.show()
+        cases_today = dane[2]
+        if country not in cases.keys():
+            cases[country] = {}
+        cases[country][date] = cases_today
+
+d = input("> ")
+if d in kraje:
+    x = cases[d].keys()
+    y = cases[d].values()
+    plt.plot(x, y)
+    plt.xlabel("data")
+    plt.ylabel("ilość zachorowań")
+    plt.show()
+else:
+    print("Nie ma takiego kraju!")
